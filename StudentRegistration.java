f470098a63296325ae376e4ffe34bfa6993cc5ca
Omar Bakry <67487202+0xbakry@users.noreply.github.com>
2023-05-23 22:29:53 +0300

    Update StudentRegistration.java

--- StudentRegistration.java
+++ StudentRegistration.java
@@ -2,23 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.studentregistration;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  *
- * @author omarb
+ * @author omarbakry
  */
 public class StudentRegistration {
 
     private static List<Student> students = new ArrayList<>();
-    private static final String BATCH_DATA_FOLDER = "E:\\FCI_Tasks\\Cloud\\app\\data\\batch\\";
-    
+    private static final String BATCH_DATA_FOLDER = "/app/data/batch/";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -39,6 +39,11 @@ public class StudentRegistration {
                 case 2:
                     addBatchStudentsData(scanner);
                     break;
+                case 3:
+                    for (int i = 0; i < students.size(); i++) {
+                        System.out.print(students.get(i).toString());
+                    }
+                    break;
                 case 0:
                     System.out.println("Exiting the program...");
                     break;
@@ -51,21 +56,46 @@ public class StudentRegistration {
 
         scanner.close();
     }
+
     private static void addStudentData(Scanner scanner) {
-        scanner.nextLine(); 
+        scanner.nextLine();
         System.out.println("Enter the students data in the following order (NAME, ID, COURSES).");
         String name = scanner.nextLine();
         int id = scanner.nextInt();
-        scanner.nextLine(); 
+        scanner.nextLine();
         String coursesInput = scanner.nextLine();
         String[] courses = coursesInput.split(",");
         Student student = new Student(name, id, courses);
-        students.add(student);
+
+        // Write student data to the CSV file
+        writeStudentDataToCSV(student);
 
         System.out.println("Student data added successfully.");
     }
 
-      private static void addBatchStudentsData(Scanner scanner) {
+    private static void writeStudentDataToCSV(Student student) {
+        try ( FileWriter writer = new FileWriter("/app/data/batch/data.csv", true)) {
+            // Append the student data to the CSV file
+            writer.append(student.getName())
+                    .append(";")
+                    .append(String.valueOf(student.getId()))
+                    .append(";");
+
+            String[] courses = student.getCourses();
+            for (int i = 0; i < courses.length; i++) {
+                writer.append(courses[i]);
+                if (i < courses.length - 1) {
+                    writer.append(",");
+                }
+            }
+
+            writer.append("\n");
+        } catch (IOException e) {
+            System.out.println("Error writing student data to CSV file: " + e.getMessage());
+        }
+    }
+
+    private static void addBatchStudentsData(Scanner scanner) {
         File[] batchFiles = getEligibleBatchFiles();
         int numFiles = batchFiles.length;
 
@@ -78,7 +108,7 @@ public class StudentRegistration {
         for (int i = 0; i < numFiles; i++) {
             System.out.println((i + 1) + ". " + batchFiles[i].getName());
         }
-        
+
         scanner.nextLine();
         System.out.print("Enter the name of the file to insert the students' info: ");
         String fileName = scanner.nextLine();
@@ -104,8 +134,33 @@ public class StudentRegistration {
             return;
         }
 
-        students.addAll(batchStudents);
+        // Write batch students' data to the CSV file
+        writeBatchStudentsDataToCSV(batchStudents);
+
         System.out.println("Batch student data added successfully.");
+    }
+
+    private static void writeBatchStudentsDataToCSV(List<Student> students) {
+        try ( FileWriter writer = new FileWriter("data.csv", true)) {
+            for (Student student : students) {
+                writer.append(student.getName())
+                        .append(";")
+                        .append(String.valueOf(student.getId()))
+                        .append(";");
+
+                String[] courses = student.getCourses();
+                for (int i = 0; i < courses.length; i++) {
+                    writer.append(courses[i]);
+                    if (i < courses.length - 1) {
+                        writer.append(",");
+                    }
+                }
+
+                writer.append("\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing batch students' data to CSV file: " + e.getMessage());
+        }
     }
 
     private static File[] getEligibleBatchFiles() {
@@ -127,7 +182,7 @@ public class StudentRegistration {
     private static List<Student> readBatchStudentsData(File file) {
         List<Student> batchStudents = new ArrayList<>();
 
-        try (Scanner scanner = new Scanner(file)) {
+        try ( Scanner scanner = new Scanner(file)) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] studentData = line.split(";");
@@ -147,8 +202,8 @@ public class StudentRegistration {
 
         return batchStudents;
 
-}
-    public static class Student {
+    }
+public static class Student {
 
     private String name;
     private int id;
@@ -185,4 +240,6 @@ public class StudentRegistration {
         this.courses = courses;
     }
 }
+
 }
+
